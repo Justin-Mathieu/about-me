3edd20840dbe2f9e8c761fb015e3af5ac43684cc
Justin <justinmathiue0@gmail.com>
2022-02-19 21:12:52 -0800

    added extension to about me

--- js/app.js
+++ js/app.js
@@ -1,85 +1,84 @@
-"use strict";
+'use strict';
 
-let username = prompt("What is your name?");
-confirm("Hi " + username + " Answer some of these questions about me!");
+let username = prompt('What is your name?');
+confirm('Hi ' + username + ' Answer some of these questions about me!');
 
-let dogname = prompt("Is my dog named Larry");
-if (dogname === "yes") {
+let dogname = prompt('Is my dog named Larry');
+if (dogname === 'yes') {
   //   console.log('Incorrect!')
-  alert("Incorrect!");
-} else if (dogname === "no") {
+  alert('Incorrect!');
+} else if (dogname === 'no') {
   //console.log('Correct!')
-  alert("Correct!");
+  alert('Correct!');
 }
 //console.log('That is not an answer')
 else {
-  alert("That is not an answer!");
+  alert('That is not an answer!');
 }
 
-let place = prompt("Do I live in vancouver, WA?");
-if (place === "yes") {
+let place = prompt('Do I live in vancouver, WA?');
+if (place === 'yes') {
   //console.log('Correct)
-  alert("Correct");
-} else if (place === "no") {
+  alert('Correct');
+} else if (place === 'no') {
   //console.log('Incorrect!')
-  alert("Incorrect");
+  alert('Incorrect');
 }
 //console.log('Please answer yes or no')
 else {
   alert('Please answer "yes" or "no"!');
 }
 
-let food = prompt("Is my favorite food pizza?");
-if (food === "yes") {
+let food = prompt('Is my favorite food pizza?');
+if (food === 'yes') {
   //console.log('Incorrect!')
-  alert("Incorrect!");
-} else if (food === "no") {
+  alert('Incorrect!');
+} else if (food === 'no') {
   //console.log('Correct!')
-  alert("Correct!");
+  alert('Correct!');
 } else {
   //console.log('Please enter yes or no)
   alert('Please enter "yes" or "no"');
 }
 
-let color = prompt("Is my favorite color red?");
-if (color === "yes") {
+let color = prompt('Is my favorite color red?');
+if (color === 'yes') {
   //console.log('Correct!)
-  alert("Correct!");
-} else if (color === "no") {
+  alert('Correct!');
+} else if (color === 'no') {
   //console.log('Incorrect!')
-  alert("Incorrect!");
+  alert('Incorrect!');
 } else {
   //console.log('yes or no please')
   alert('"yes" or "no" please!');
 }
 
-let music = prompt("Is jazz my favorite genre of music?");
-if (music === "yes") {
+let music = prompt('Is jazz my favorite genre of music?');
+if (music === 'yes') {
   //console.log('Incorrect!')
-  alert("Incorrect!");
-} else if (music === "no") {
+  alert('Incorrect!');
+} else if (music === 'no') {
   //console.log('Correct')
-  alert("Correct!");
+  alert('Correct!');
 } else {
   //console.log('yes or no')
   alert('"yes" or"no"');
 }
 //console.log('Thankyou!)
-alert("Thank you!" + username);
+alert('Thank you!' + username);
 
-let favcolor = ["red", "green", "blue"];
+let favcolor = ['red', 'green', 'blue'];
 //let favcolor = "red";
 let maxAttempts = 7;
 let currentAttempt = 0;
 while (currentAttempt < maxAttempts) {
-  let response = prompt("guess a color");
+  let response = prompt('guess a color');
   //for (let i = 0; i < favcolor.length; i++) {
   if (response === favcolor) {
-    alert("Correct!");
+    alert('Correct!');
     break;
   }
   alert(`Incorrect! ${maxAttempts - currentAttempt - 1} attempts remaning`);
 
-  //}
   currentAttempt++;
 }
